962d54826371a09151e15ba6507c69b5f8d77e71
Basel <basel.ihab77@gmail.com>
2018-08-26 17:13:23 +0200

    Fix bug with cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
+var user;
 
 
 // Create and Deploy Your First Cloud Functions
@@ -10,12 +11,13 @@ admin.initializeApp();
 //   console.log("Hello!")
 //  response.send("Hello World!");
 // });
-exports.updateXP = functions.database.ref('{userID}/items/{todoID}/pomodoros')
+exports.updateXP = functions.database.ref('/{userID}/items/{todoID}/pomodoros')
 .onUpdate((change, context) => {
   const before= change.before.val();
   const after= change.after.val();
+  user = context.params.userID;
   if(after<before){
-  return admin.database().ref('/xp').once('value')
+  return admin.database().ref('/'+user+'/xp').once('value')
     .then(snapshot =>{
     var xp = snapshot.val();
     xp=parseInt(xp)+10;
@@ -23,7 +25,7 @@ exports.updateXP = functions.database.ref('{userID}/items/{todoID}/pomodoros')
     })
     .then(result =>{
       const xp_count= parseInt(result);
-      return admin.database().ref('/items').parent.update({"xp": xp_count});
+      return admin.database().ref('/'+user).update({"xp": xp_count});
     })
     .catch(error =>{
       console.log(error);
