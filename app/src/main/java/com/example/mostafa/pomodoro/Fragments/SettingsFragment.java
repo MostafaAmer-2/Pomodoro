ce28c4efd90e29be31a6b592cdad05340ef695b4
Basel <basel.ihab77@gmail.com>
2018-08-29 13:32:51 +0200

    Setup contact us button

--- app/src/main/java/com/example/mostafa/pomodoro/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/mostafa/pomodoro/Fragments/SettingsFragment.java
@@ -6,9 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.mostafa.pomodoro.Activities.AuthenticationActivity;
@@ -16,8 +14,6 @@ import com.example.mostafa.pomodoro.R;
 import com.example.mostafa.pomodoro.Settings.Preferences;
 import com.facebook.login.LoginManager;
 import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,34 +25,48 @@ public class SettingsFragment extends Fragment {
     @BindView(R.id.logout)
     TextView logout;
     @BindView(R.id.contactUs)
-    TextView contactUs;
+    TextView contactUsButton;
+    @BindView(R.id.backButton)
+    Button backButton;
+    @BindView(R.id.settingsLayout)
+    View settingsLayout;
+    @BindView(R.id.contactUsLayout)
+    View contactUsLayout;
 
     Realm realm;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
         ButterKnife.bind(this, view);
         realm = Realm.getDefaultInstance();
-
+        settingsLayout.setVisibility(View.VISIBLE);
+        contactUsLayout.setVisibility(View.GONE);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 LoginManager.getInstance().logOut();
-                Preferences.saveUserID(getActivity().getApplicationContext(),"");
+                Preferences.saveUserID(getActivity().getApplicationContext(), "");
                 realm.beginTransaction();
                 realm.deleteAll();
                 realm.commitTransaction();
                 goToAuthentication();
             }
         });
-
-        contactUs.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                settingsLayout.setVisibility(View.VISIBLE);
+                contactUsLayout.setVisibility(View.GONE);
+            }
+        });
+        contactUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                settingsLayout.setVisibility(View.GONE);
+                contactUsLayout.setVisibility(View.VISIBLE);
             }
         });
         return view;
